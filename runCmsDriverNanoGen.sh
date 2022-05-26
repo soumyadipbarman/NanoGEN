3b9a5e6dfac5a57d6d011328063f901f9383906a
soumyadipbarman <barmansoumyadip@gmail.com>
2022-05-26 17:31:47 +0200

    updated

--- runCmsDriverNanoGen.sh
+++ runCmsDriverNanoGen.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-#TODO: Make this a proper script that also creates a crab_submit file
 if [[ $# -ne 2 ]]; then
     echo "Must have two arguments: runCmsDriverNanoGen.sh <config fragment> <outputfile.root>"
     exit 1
@@ -8,7 +7,7 @@ fi
 
 fragment=${1/python\//}
 
-cmsDriver.py Configuration/GenProduction/python/fragment/$fragment \
+cmsDriver.py Configuration/NanoGEN/python/$fragment \
     --fileout file:$2 --mc --eventcontent NANOAODSIM \
     --datatier NANOAOD --conditions auto:mc --step LHE,GEN,NANOGEN \
     --python_filename configs/${fragment/cff/cfg} \
