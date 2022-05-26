01d4636f6a673b26dd75cb105bcfffd7c0f82c77
soumyadipbarman <barmansoumyadip@gmail.com>
2022-05-26 19:30:05 +0200

    remove customise option

--- runCmsDriverNanoGen.sh
+++ runCmsDriverNanoGen.sh
@@ -5,11 +5,15 @@ if [[ $# -ne 2 ]]; then
     exit 1
 fi
 
+#customize="--customise_commands process.RandomNumberGeneratorService.externalLHEProducer.initialSeed=999"  #Kenneth 
+#customize="--customise_commands process.source.firstRun=cms.untracked.uint32(1)"
+#customize="--customise_commands "process.source.firstRun=cms.untracked.uint32(1)"
+
 fragment=${1/python\//}
 
 cmsDriver.py Configuration/NanoGEN/python/$fragment \
     --fileout file:$2 --mc --eventcontent NANOAODSIM \
     --datatier NANOAOD --conditions auto:mc --step LHE,GEN,NANOGEN \
     --python_filename configs/${fragment/cff/cfg} \
-    --customise_commands "process.source.firstRun=cms.untracked.uint32(1)" \
+    #$customize \
     -n 100 --no_exec
